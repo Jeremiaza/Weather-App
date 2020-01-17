01acd01845967962d98a55579ca2a5161b33365e
Jeremiaza <jeremias.korhonen@live.fi>
2020-01-17 19:07:24 +0200

    First push

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
+const appId = process.env.APPID;
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
@@ -16,17 +16,32 @@ const app = new Koa();
 app.use(cors());
 
 const fetchWeather = async () => {
-  const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
+  const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}`;
   const response = await fetch(endpoint);
 
   return response ? response.json() : {}
+};
+const fetchMyWeather = async (lon, lat) => {
+  const endpoint = `${mapURI}/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+  const response = await fetch(endpoint);
+
+  return response ? response.json() : {};
 };
 
 router.get('/api/weather', async ctx => {
   const weatherData = await fetchWeather();
 
   ctx.type = 'application/json; charset=utf-8';
-  ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+  ctx.body = weatherData ? weatherData : {};
+});
+
+router.get('/api/myweather', async ctx => {
+  if (ctx.request.query.lon && ctx.request.query.lat) {
+    const { lon, lat, } = ctx.request.query;
+    const weatherData = await fetchMyWeather(lon, lat);
+    ctx.type = 'application/json; charset=utf-8';
+    ctx.body = weatherData.weather ? weatherData : {};
+  }
 });
 
 app.use(router.routes());
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Geolocation from 'react-geolocation';
 
-const baseURL = process.env.ENDPOINT;
+const baseURL = "http://localhost:9000/api";
 
 const getWeatherFromApi = async () => {
   try {
@@ -13,33 +14,84 @@ const getWeatherFromApi = async () => {
 
   return {};
 };
+const getMyWeatherFromApi = async (latitude, longitude) => {
+  try {
+    const response = await fetch(`${baseURL}/myweather?lon=${longitude}&lat=${latitude}`);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+
+  return {};
+}
 
 class Weather extends React.Component {
   constructor(props) {
-    super(props);
 
+    super(props);
+    this.getWeather = this.getWeather.bind(this);
     this.state = {
-      icon: "",
+      helsinki_icon: "",
+      helsinki_temp: "",
+      my_icon: "",
+      my_temp: "",
+      lat: "",
+      lon: ""
     };
   }
-
-  async componentWillMount() {
+  async getWeather(latitude, longitude) {
     const weather = await getWeatherFromApi();
-    this.setState({icon: weather.icon.slice(0, -1)});
-  }
+    const myweather = await getMyWeatherFromApi(latitude, longitude);
+    console.log(weather);
+    console.log(myweather);
+
+    this.setState({
+      helsinki_icon: weather.weather[0].icon,
+      helsinki_temp: weather.main.temp - 273.15,
+      my_icon: myweather.weather[0].icon,
+      my_temp: myweather.main.temp - 273.15,
+      my_city: myweather.name,
+      lat: latitude,
+      lon: longitude
+    });
+  };
 
   render() {
-    const { icon } = this.state;
+    const { helsinki_icon, helsinki_temp, my_icon, my_temp, my_city, lat, lon } = this.state;
+    var roundedtemp = Math.round(Number(helsinki_temp) * 10) / 10;
+    var myroundedtemp = Math.round(Number(my_temp) * 10) / 10;
 
     return (
       <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} /> }
+        <Geolocation
+          render={({
+            fetchingPosition,
+            position: { coords: { latitude, longitude } = {} } = {},
+            error,
+            getCurrentPosition
+          }) =>
+            <div>
+              <button onClick={getCurrentPosition}>Update Position</button>
+              {error &&
+                <div>
+                  {error.message}
+                </div>}
+              <pre>
+                latitude: {latitude}
+              </pre>
+              <pre>
+                longitude: {longitude}
+              </pre>
+              <button onClick={async () => { await this.getWeather(latitude, longitude); }}>Get the weather!</button>
+            </div>}
+        />
+        <h3>Weather in Helsinki</h3>
+        <div>{helsinki_icon && <img src={`http://openweathermap.org/img/wn/${helsinki_icon}.png`} />} <b>{roundedtemp}C</b></div>
+        <h3>Weather in {my_city || "not found"}</h3>
+        <div>{my_icon && <img src={`http://openweathermap.org/img/wn/${my_icon}.png`} />} <b>{myroundedtemp}C</b></div>
       </div>
     );
   }
 }
+ReactDOM.render(<Weather />, document.getElementById('app'));
 
-ReactDOM.render(
-  <Weather />,
-  document.getElementById('app')
-);
